89d91ae4b3325de60a32c8885207c1c24e01ac4b
franks42 <frank.siebenlist@gmail.com>
2013-01-08 23:07:16 -0800

    managed to get the basics working todo: multiple watch-fns per map-key          implement keys with __iterate__ to be able to use higher order fns

--- src/shoreleave/browser/storage/localstorage.cljs
+++ src/shoreleave/browser/storage/localstorage.cljs
@@ -1,7 +1,7 @@
 (ns shoreleave.browser.storage.localstorage
   "An idiomatic interface to the browser's local storage"
   (:require [cljs.reader :as reader]
-            [goog.storage.mechanism.HTML5LocalStorage :as hml5ls]))
+            [goog.storage.mechanism.HTML5LocalStorage :as html5ls]))
 
 ;; Watchers
 ;; --------
@@ -10,7 +10,8 @@
 ;; To support this, Shoreleave's local storage use IWatchable and maintains
 ;; the watchers in an atom.
 
-(def ls-watchers {})
+(def ls-watchers (atom {}))
+(def ls-map-watchers (atom {}))
 
 ;; `localStorage` support
 ;; ----------------------
@@ -28,6 +29,13 @@
 ;;  * `(dissoc! local-storage :saved-results)` - remove an item
 ;;  * `(empty! local-storage)` - Clear out the localStorage store
 
+(defprotocol ITransientMapWatchable
+  (-notify-map-watches [this map-key oldval newval])
+  (-add-map-watch [this map-key fn-key f])
+  (-remove-map-watch [this map-key fn-key])
+  (-remove-all-map-watch [this map-key]))
+
+
 (extend-type goog.storage.mechanism.HTML5LocalStorage
   
   ILookup
@@ -35,10 +43,13 @@
     ([ls k]
       (-lookup ls k nil))
     ([ls k not-found]
-      (reader/read-string (.get ls (name k) not-found))))
+      (if-let [v (.get ls (pr-str k))]
+        (cljs.reader/read-string v)
+        not-found)))
 
   ICounted
-  (-count  [ls] (.getCount ls))
+  (-count [ls] 
+    (.getCount ls))
 
   IFn
   (-invoke
@@ -49,20 +60,53 @@
 
   ITransientAssociative
   (-assoc! [ls k v]
-    (.set ls (name k) (pr-str v)))
+    (let [oldval (get ls k nil)]
+      (when-not (= oldval v)
+        (.set ls (pr-str k) (pr-str v))
+        ;; next is a hack to communicate the key to the notify-watches context
+        ;; protocol doesn't really match well, but this way it "works"
+        (-notify-watches ls {:key k :value oldval} {:key k :value v})))
+    ls)
 
   ITransientMap
   (-dissoc! [ls k]
-    (.remove ls (name k)))
+    (let [oldval (get ls k nil)]
+      (when-not (nil? oldval)
+        (.remove ls (pr-str k))
+        ;; next is a hack to communicate the key to the notify-watches context
+        ;; protocol doesn't really match well, but this way it "works"
+        (-notify-watches ls {:key k :value oldval} {:key k :value nil})))
+    ls)
 
+  ;; ITransientCollection
+
+  ;; ough... jumping thru hoops to fulfill the IWatchable protocol requirements
+  ;; the storage lookup key is not the same as the IWatchable's fn-key...
+  ;; TODO: need to be able to add multiple watchers per key
   IWatchable
   (-notify-watches [ls oldval newval]
-    (doseq  [[key f] ls-watchers]
-      (f key ls oldval newval)))
+    (when-let [f (get @ls-watchers (:key oldval) nil)]
+      (f (:key oldval) ls (:value oldval) (:value newval)))
+    ls)
   (-add-watch [ls key f]
     (swap! ls-watchers assoc key f))
   (-remove-watch [ls key]
     (swap! ls-watchers dissoc key))
+
+  ITransientMapWatchable
+  (-notify-map-watches [ls map-key oldval newval]
+    (when-let [fns-map (get @ls-map-watchers map-key nil)]
+      (doseq [k-f fns-map]
+        ((:val k-f) (:key k-f) ls map-key oldval newval)))
+    this)
+  (-add-map-watch [ls map-key fn-key f]
+    (swap! ls-map-watchers assoc-in [map-key fn-key] f))
+  (-remove-map-watch [ls map-key fn-key]
+    (let [fns-map (get-in @ls-map-watchers [map-key])
+          new-fns-map (dissoc fns-map fn-key)]
+      (swap! ls-map-watchers assoc-in [map-key] new-fns-map)))
+  (-remove-all-map-watch [ls map-key]
+    (swap! ls-map-watchers dissoc map-key))
 
   ;IPrintable
   ;(-pr-seq  [c opts]
